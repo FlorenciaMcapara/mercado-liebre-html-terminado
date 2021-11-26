682af6529a1bebeb8a9796ea3b43b66f848c836b
FlorenciaMcapara <flornatura2903@gmail.com>
2021-11-26 20:44:04 -0300

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PublicPath = path.resolve(__dirname, './public');
+app.use(express.static(PublicPath));
+
+app.listen(3000, () => {
+    console.log('servidor corriendo en el puerto 3000');
+    
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    
+})
