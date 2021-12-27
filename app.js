c00b1dd42aa8a4379e306ce1f193f42190cf5a56
FlorenciaMcapara <flornatura2903@gmail.com>
2021-12-27 11:15:09 -0300

    router and form

--- app.js
+++ app.js
@@ -14,4 +14,9 @@ app.listen(3000, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
     
-})
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+
+});
