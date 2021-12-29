8b446a5f935565d6a42bfbc239778a1673492649
FlorenciaMcapara <94146915+FlorenciaMcapara@users.noreply.github.com>
2021-12-28 23:13:22 -0300

    login

--- app.js
+++ app.js
@@ -20,3 +20,8 @@ app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 
 });
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+
+});
